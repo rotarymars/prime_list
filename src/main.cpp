d6aa4a2178149fdcd3dccaf8a87696b509a84552
rotarymars <s.goto2050@gmail.com>
2025-05-17 18:16:29 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,7 @@ int main() {
     std::smatch match;
     if (std::regex_search(filename, match, number_pattern)) {
       filenames.push_back(filename);
+      filecntmax = std::max(filecntmax, match[1]);
     }
   }
   std::sort(filenames.begin(), filenames.end(), [&](const std::string& a, const std::string& b) {
